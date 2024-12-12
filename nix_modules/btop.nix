25aaf50ff8bc506cab1711e1c78d478d5ddac6b5
dfosterj <dfosterj@hotmail.com>
2024-12-11 18:34:35 -0600

    use btop nord

--- nix_modules/btop.nix
+++ nix_modules/btop.nix
@@ -6,6 +6,6 @@
   };
 
   # home.file.".config/btop/themes/catppuccin-mocha.rasi".text = builtins.readFile ../rofi/config.rasi;
-  home.file.".config/btop/themes/catppuccin_mocha.theme".text =
+  home.file.".config/btop/themes/nord.theme".text =
     builtins.readFile ../btop/themes/catppuccin_mocha.theme;
 }
