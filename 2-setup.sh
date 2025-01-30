aeefed02978ea24c1d68effc7f449a412397b190
dfosterj <dfosterj@hotmail.com>
2025-01-30 10:24:53 -0600

    test no nixGL preload

--- 2-setup.sh
+++ 2-setup.sh
@@ -2,13 +2,13 @@
 
 # Add the Home Manager channel
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+# install nixGL
+nix-channel --add https://github.com/nix-community/nixGL/archive/main.tar.gz nixgl && nix-channel --update
+# nix-env -iA nixgl.auto.nixGLDefault
 
 # Update all Nix channels
 nix-channel --update
 
-# install nixGL
-nix-channel --add https://github.com/nix-community/nixGL/archive/main.tar.gz nixgl && nix-channel --update
-nix-env -iA nixgl.auto.nixGLDefault
 
 # Install Home Manager
 nix-shell '<home-manager>' -A install
