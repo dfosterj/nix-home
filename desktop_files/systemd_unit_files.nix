ed80922207b434ac3ee464938a3d0a950c67ffda
dfosterj <dfosterj@hotmail.com>
2025-01-31 16:16:08 -0600

    fix sys d

--- desktop_files/systemd_unit_files.nix
+++ desktop_files/systemd_unit_files.nix
@@ -3,13 +3,13 @@
 {
   systemd.user.services.dotfile-link-check = {
     Unit = {
-      Description = "Dotfiles Link Check Service for %i";
+      Description = "Dotfiles Link Check Service for %u";
       After = [ "network.target" ];
     };
 
     Service = {
       Type = "simple";
-      ExecStart = "${pkgs.bash}/bin/bash -c 'while true; do $HOME/.dotfiles/bin/systemd_dotfile_link_check_service.sh >> /var/log/dotfiles_%i.log 2>&1; sleep 300; done'";
+      ExecStart = "${pkgs.bash}/bin/bash -c 'while true; do $HOME/.dotfiles/bin/systemd_dotfile_link_check_service.sh >> /var/log/dotfiles_%u.log 2>&1; sleep 300; done'";
       Restart = "on-failure";
     };
 
