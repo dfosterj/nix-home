d3d97c93d018d10d502e62bb341c2a99fca8f64e
dfosterj <dfosterj@hotmail.com>
2025-01-31 23:21:58 -0600

    godotfix

--- desktop_files/desktop_entries.nix
+++ desktop_files/desktop_entries.nix
@@ -1,11 +1,4 @@
 { ... }: {
-  # Override the Kitty desktop entry
-  xdg.desktopEntries.kitty = {
-	name = "kitty";
-    exec = "nixGL kitty";
-    icon = "kitty";
-    };
-
   # Override the Alacritty desktop entry
   xdg.desktopEntries.Alacritty = {
     name = "Alacritty";
@@ -14,11 +7,27 @@
   };
 
   # Override the Neovide desktop entry
+  xdg.desktopEntries.godot4 = {
+    name = "godot4";
+    exec = "nixVulkanIntel godot4";
+    icon = "godot4";
+  };
+
+  # Override the Kitty desktop entry
+  xdg.desktopEntries.kitty = {
+	name = "kitty";
+    exec = "nixGL kitty";
+    icon = "kitty";
+    };
+
+
+  # Override the Neovide desktop entry
   xdg.desktopEntries.neovide = {
     name = "neovide";
     exec = "nixGL neovide";
     icon = "neovide";
   };
+
 
   # Override the WezTerm desktop entry
   xdg.desktopEntries."org.wezfurlong.wezterm" = {
