be3b8f0ae0bd0e0aba1857686e9a43a1a88f757d
David Foster <dfosterj@pm.me>
2025-02-13 18:08:11 -0600

    fish it up

--- home.nix
+++ home.nix
@@ -22,12 +22,20 @@
     NIXPKGS_ALLOW_UNFREE = 1;
     GEM_HOME = "${config.home.homeDirectory}/.rubygems";
     PATH = "${pkgs.ruby}/bin:${config.home.homeDirectory}/.rubygems/bin:$PATH";
+    SHELL = "${pkgs.fish}/bin/fish";
   };
 
   programs.git = {
     enable = true;
     userName = "David Foster";
     userEmail = "dfosterj@pm.me";
+  };
+
+  programs.fish = {
+    shellAliases = {
+      nt = "sudo nixos-rebuild test --flake /etc/nixos#default";
+      ns = "sudo nixos-rebuild switch --flake /etc/nixos#default";
+    };
   };
 
   home.file.".gemrc".text = ''
